5b5cbe337f7cd315d056604e1bb2ac3943698ff7
Julius Pedersen <julius@tuuturu.io>
2025-03-02 14:09:11 +0100

    🐛 Fix section headers missing

--- pkg/html/helpers.go
+++ pkg/html/helpers.go
@@ -55,28 +55,6 @@ func extractNavigation(root *html.Node) ([]Link, error) {
 	return links, nil
 }
 
-func extractSections(root *html.Node) ([]Section, error) {
-	sectionTags, err := querySelect(root, "section")
-	if err != nil {
-		return nil, fmt.Errorf("querying <section>'s: %w", err)
-	}
-
-	sections := make([]Section, len(sectionTags))
-	for index, tag := range sectionTags {
-		paragraphs, err := extractParagraphs(tag)
-		if err != nil {
-			return nil, fmt.Errorf("extracting paragraphs: %w", err)
-		}
-
-		sections[index] = Section{
-			Header:     tag.FirstChild.Data,
-			Paragraphs: paragraphs,
-		}
-	}
-
-	return sections, nil
-}
-
 func extractPageHeader(root *html.Node) (string, error) {
 	mains, err := querySelect(root, "main")
 	if err != nil {
@@ -89,6 +67,47 @@ func extractPageHeader(root *html.Node) (string, error) {
 	}
 
 	return headers[0].FirstChild.Data, nil
+}
+
+func extractSections(root *html.Node) ([]Section, error) {
+	sectionTags, err := querySelect(root, "section")
+	if err != nil {
+		return nil, fmt.Errorf("querying <section>'s: %w", err)
+	}
+
+	sections := make([]Section, len(sectionTags))
+	for index, tag := range sectionTags {
+		title, err := extractHeader(tag)
+		if err != nil {
+			return nil, fmt.Errorf("extracting section header: %w", err)
+		}
+
+		paragraphs, err := extractParagraphs(tag)
+		if err != nil {
+			return nil, fmt.Errorf("extracting paragraphs: %w", err)
+		}
+
+		sections[index] = Section{
+			Header:     title,
+			Paragraphs: paragraphs,
+		}
+	}
+
+	return sections, nil
+}
+
+func extractHeader(root *html.Node) (string, error) {
+	for _, tag := range []string{"h1", "h2", "h3", "h4", "h5"} {
+		elements, _ := querySelect(root, tag)
+
+		if len(elements) == 0 {
+			continue
+		}
+
+		return elements[0].FirstChild.Data, nil
+	}
+
+	return "", errors.New("found no headers")
 }
 
 func extractParagraphs(parent *html.Node) ([]string, error) {
